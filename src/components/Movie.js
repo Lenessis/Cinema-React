c67dc6a5a422e67b91f6209a6e6c42b829778b05
Lenessis <mkorecka@gmail.com>
2021-12-23 22:17:57 +0100

    Edit Movie i Movies component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -12,7 +12,7 @@ const Movie = props =>
     return(
         <div className="card" >
             <img src={props.img} className="card-img-top" alt="{props.title}" />
-            <div class="card-body">
+            <div className="card-body">
                 <h5 className="card-title">{props.title} ({props.productionYear.getFullYear()})</h5>
                 <p> Duration: {props.time} minutes</p>
                 <p className="card-text"> {props.description}</p>
@@ -31,12 +31,26 @@ const Movie = props =>
 
 Movie.protoTypes =
 {
+    /*id(props, propName)
+    {
+        if(!props[propName])
+        {
+            return new Error(propName + "doesn't exist")
+        }
+    },*/
+
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     img: PropTypes.string,
-    productionYear: PropTypes.object,
+    productionYear: PropTypes.instanceOf(Date),
     time: PropTypes.number.isRequired,
     description: PropTypes.string
+}
+
+Movie.defaultProps=
+{
+    img: "https://icon-library.com/images/none-icon/none-icon-0.jpg",
+    description: "No description"
 }
 
 export default Movie;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -10,6 +10,7 @@ import '../stylesheets/Movies.css';
 var movieList = [
     
     {
+        id: 1,
         title: "Hobbit",
         img: "https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/original/products/86974/86527/the_hobbit_the_desolution_of_smaug__final_poster_buy_original_movie_posters_at_starstills__35272__21743.1394515789.jpg?c=2",
         productionYear: new Date("2014"),
@@ -17,22 +18,31 @@ var movieList = [
         description: "The films take place in the fictional world of Middle-earth sixty years before the beginning of The Lord of the Rings, and follow hobbit Bilbo Baggins (Martin Freeman), who is convinced by the wizard Gandalf the Grey (Ian McKellen) to accompany thirteen dwarves, led by Thorin Oakenshield (Richard Armitage), on a quest."
     },
     {
+        //id: 2,
         title: "Harry Potter",
         img: "https://fwcdn.pl/fpo/05/71/30571/7529392.3.jpg",
         productionYear: new Date("2007"),
         time: 120,
         description: "The films take place in the fictional world of Middle-earth sixty years before the beginning of The Lord of the Rings, and follow hobbit Bilbo Baggins (Martin Freeman), who is convinced by the wizard Gandalf the Grey (Ian McKellen) to accompany thirteen dwarves, led by Thorin Oakenshield (Richard Armitage), on a quest."
     },
+    {
+        id: 3,
+        title: "Rudolf",
+        productionYear: new Date("1989"),
+        time: 90
+    },
     
 ]
 
-const Movies =()=>
+const Movies = () =>
 {
     return (
         <div className="album">
-            {movieList.map(movie =>{
+            {movieList.map((movie, key) =>{
                 return(
                     <Movie 
+                    key={key}  
+                    id = {movie.id}
                     title = {movie.title}
                     img = {movie.img}
                     productionYear = {movie.productionYear}
